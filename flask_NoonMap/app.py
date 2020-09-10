a710d251cc35120359bdcca370dcda267b3ea59f
ShinEunChae <68213799+ShinEunChae@users.noreply.github.com>
2020-09-10 23:50:28 +0900

    input data for model

--- flask_NoonMap/app.py
+++ flask_NoonMap/app.py
@@ -3,6 +3,10 @@ import json
 from flask import Flask, render_template, Markup, request
 from flask_sqlalchemy import SQLAlchemy
 from folium.plugins import MarkerCluster
+from datetime import datetime
+import rescaler
+import numpy as np
+import pandas as pd
 
 app = Flask(__name__)  # bridge.db, rain_msg.db 연동
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///bridge_db.db"
@@ -30,9 +34,16 @@ class Bridge(db.Model):
     wl_station_code = db.Column(db.Integer)
     station_name = db.Column(db.Text)
     location = db.Column(db.Text)
-    obs_date = db.Column(db.Text)
+    obs_date = db.Column(db.DateTime)
     WL = db.Column(db.Float)
     bridge_height = db.Column(db.Float)
+
+
+class Temp(db.Model):
+    __tablename__ = 'TEMP'
+    __bind_key__ = 'bridge_key'
+    obs_date = db.Column(db.DateTime, primary_key=True, index=True)
+    WL = db.Column(db.Float)
 
 
 class Rain_msg(db.Model):
@@ -58,16 +69,50 @@ db.create_all(bind='rain_key')
 
 
 list_seoul = db.session.query(Bridge.latitude, Bridge.longitude,
-                                 Bridge.bridge_name, Bridge.bridge_height, Bridge.WL, Bridge.address,
-                                 Bridge.etc_address).group_by(Bridge.location_start).filter_by(address="서울특별시").all()
+                              Bridge.bridge_name, Bridge.bridge_height, Bridge.obs_date, Bridge.WL,
+                              Bridge.address, Bridge.etc_address).group_by(Bridge.location_start).filter_by(address="서울특별시").all()
 
 list_incheon = db.session.query(Bridge.latitude, Bridge.longitude,
-                                 Bridge.bridge_name, Bridge.bridge_height, Bridge.WL, Bridge.address,
-                                 Bridge.etc_address).group_by(Bridge.location_start).filter_by(address="인천광역시").all()
+                              Bridge.bridge_name, Bridge.bridge_height, Bridge.obs_date, Bridge.WL,
+                              Bridge.address, Bridge.etc_address).group_by(Bridge.location_start).filter_by(address="인천광역시").all()
 
 list_gyeonggi = db.session.query(Bridge.latitude, Bridge.longitude,
-                                 Bridge.bridge_name, Bridge.bridge_height, Bridge.WL, Bridge.address,
-                                 Bridge.etc_address).group_by(Bridge.location_start).filter_by(address="경기도").all()
+                              Bridge.bridge_name, Bridge.bridge_height, Bridge.obs_date, Bridge.WL,
+                              Bridge.address, Bridge.etc_address).group_by(Bridge.location_start).filter_by(address="경기도").all()
+
+brid_list = [ p for (p,) in db.session.query(Bridge.location_start).group_by(Bridge.location_start)] # 교량 목록
+
+
+# MinMaxScaler가 없어서 대체
+def rescale(input_list, newmin, newmax):
+    oldmin = np.min(input_list)
+    oldmax = np.max(input_list)
+    oldrange = (oldmax - oldmin)
+    newrange = (newmax - newmin)
+
+    newlist = []
+    for i in input_list:
+        newi = (((i - oldmin) * newrange) / oldrange) + newmin
+        newlist.append(newi)
+
+    return newlist
+
+# 크롤링해서 db에 넣어주는 작업 필요
+# 우리는 12시간의 데이터를 쓸 거니까 -> [-12:]
+
+def input_data():
+    for i in range(1): # for i in range(len(brid_list))
+        time = [p for (p,) in db.session.query(Bridge.obs_date).filter_by(location_start=brid_list[i])]
+        value = [p for (p,) in db.session.query(Bridge.WL).filter_by(location_start=brid_list[i])]
+        std_value = rescale(value, 0, 1)
+        std_value = std_value[-12:]
+        df = pd.DataFrame({'std_value':np.array(std_value)}, index=time[-12:])
+        for j in range(12):
+            df['shift_{}'.format(j)] = df['std_value'].shift(j)
+        input_df = df.dropna().drop('std_value', axis=1)
+        input_values = input_df.values
+        input_data = input_values.reshape(input_values.shape[0], 12, 1)
+        return input_data
 
 
 #################################################################
@@ -82,6 +127,9 @@ def predict():
 def home():
     status = request.args.get('status', '0')  # sidebar menu get value
     print(status)
+    print(input_data())
+    print(type(input_data()))
+
     #############################################################################
 
     area_value = None
@@ -120,7 +168,7 @@ def home():
             print(i.latitude, i.longitude)
             text = "이름: " + str(i.bridge_name) + "\n높이: " + str(i.bridge_height) + "\n수위: " + str(i.WL)
             pp_text = folium.IFrame(text, width=100, height=150)
-            pp = folium.Popup(pp_text, max_width=400)
+            pp = folium.Popup(pp_text, max_width=1000)
             ic = folium.Icon(color='red', icon='info-sign')
             folium.Marker(
                 location=[i.latitude, i.longitude],
