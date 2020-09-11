feff3e15cd74c02c0f3cabe6f8830907e530d03e
ShinEunChae <68213799+ShinEunChae@users.noreply.github.com>
2020-09-11 17:38:28 +0900

    load model and predict WL

--- flask_NoonMap/app.py
+++ flask_NoonMap/app.py
@@ -4,9 +4,10 @@ from flask import Flask, render_template, Markup, request
 from flask_sqlalchemy import SQLAlchemy
 from folium.plugins import MarkerCluster
 from datetime import datetime
-import rescaler
 import numpy as np
 import pandas as pd
+from tensorflow import keras
+
 
 app = Flask(__name__)  # bridge.db, rain_msg.db 연동
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///bridge_db.db"
@@ -37,13 +38,6 @@ class Bridge(db.Model):
     obs_date = db.Column(db.DateTime)
     WL = db.Column(db.Float)
     bridge_height = db.Column(db.Float)
-
-
-class Temp(db.Model):
-    __tablename__ = 'TEMP'
-    __bind_key__ = 'bridge_key'
-    obs_date = db.Column(db.DateTime, primary_key=True, index=True)
-    WL = db.Column(db.Float)
 
 
 class Rain_msg(db.Model):
@@ -80,10 +74,12 @@ list_gyeonggi = db.session.query(Bridge.latitude, Bridge.longitude,
                               Bridge.bridge_name, Bridge.bridge_height, Bridge.obs_date, Bridge.WL,
                               Bridge.address, Bridge.etc_address).group_by(Bridge.location_start).filter_by(address="경기도").all()
 
-brid_list = [ p for (p,) in db.session.query(Bridge.location_start).group_by(Bridge.location_start)] # 교량 목록
+brid_list = [p for (p,) in db.session.query(Bridge.location_start).group_by(Bridge.location_start)]  # 교량 목록
+loc_seoul = [p for (p,) in db.session.query(Bridge.location_start).group_by(Bridge.location_start).filter_by(address='서울특별시')]
+loc_incheon = [p for (p,) in db.session.query(Bridge.location_start).group_by(Bridge.location_start).filter_by(address='인천광역시')]
+loc_gyeonggi = [p for (p,) in db.session.query(Bridge.location_start).group_by(Bridge.location_start).filter_by(address='경기도')]
 
 
-# MinMaxScaler가 없어서 대체
 def rescale(input_list, newmin, newmax):
     oldmin = np.min(input_list)
     oldmax = np.max(input_list)
@@ -97,38 +93,47 @@ def rescale(input_list, newmin, newmax):
 
     return newlist
 
-# 크롤링해서 db에 넣어주는 작업 필요
-# 우리는 12시간의 데이터를 쓸 거니까 -> [-12:]
 
-def input_data():
-    for i in range(1): # for i in range(len(brid_list))
-        time = [p for (p,) in db.session.query(Bridge.obs_date).filter_by(location_start=brid_list[i])]
-        value = [p for (p,) in db.session.query(Bridge.WL).filter_by(location_start=brid_list[i])]
-        std_value = rescale(value, 0, 1)
-        std_value = std_value[-12:]
-        df = pd.DataFrame({'std_value':np.array(std_value)}, index=time[-12:])
+def re_rescale(input_value, oldmin, oldmax):
+    oldrange = (oldmax - oldmin)
+    oldi = round((input_value * oldrange) + oldmin, 0)
+    return oldi
+
+
+def predict(area_value):
+    loc_area = None
+    if area_value == 'seoul':
+        loc_area = loc_seoul
+    elif area_value == 'incheon':
+        loc_area = loc_incheon
+    elif area_value == 'gyeonggi':
+        loc_area = loc_gyeonggi
+
+    prediction = []
+    for i in loc_area:
+        time = [p for (p,) in db.session.query(Bridge.obs_date).filter_by(location_start=i)][-12:]
+        value = [p for (p,) in db.session.query(Bridge.WL).filter_by(location_start=i)]
+        std_value = rescale(value, 0, 1)[-12:]
+        df = pd.DataFrame({'std_value': np.array(std_value)}, index=time)
         for j in range(12):
             df['shift_{}'.format(j)] = df['std_value'].shift(j)
         input_df = df.dropna().drop('std_value', axis=1)
         input_values = input_df.values
         input_data = input_values.reshape(input_values.shape[0], 12, 1)
-        return input_data
+        model_path = './model/' + i + '/lstm_model.hdf5'
+        new_model = keras.models.load_model(model_path)
+        new_result = new_model.predict(input_data)[0][0]
+        prediction.append(re_rescale(new_result, min(value), max(value)))
+    return prediction
 
 
 #################################################################
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    return
-
 
 @app.route('/home', methods=['GET', 'POST'])
 @app.route('/', methods=['GET', 'POST'])
 def home():
     status = request.args.get('status', '0')  # sidebar menu get value
     print(status)
-    print(input_data())
-    print(type(input_data()))
 
     #############################################################################
 
@@ -136,23 +141,34 @@ def home():
     if request.method == 'POST':
         area_value = request.form['btn_area']  # button post value
     print(area_value)
+
     #############################################################################
+
     rain = Rain_msg.query.all()  # (SELECT * FROM 테이블명)과 동일함
 
+    #############################################################################
+
     list_area = None
+    pred_wl = None
     if area_value == 'seoul':
         list_area = list_seoul
+        pred_wl = predict('seoul')
     elif area_value == 'incheon':
         list_area = list_incheon
+        pred_wl = predict('incheon')
     elif area_value == 'gyeonggi':
         list_area = list_gyeonggi
-
+        pred_wl = predict('gyeonggi')
+    print(pred_wl)
     print(list_area)  # list의 select 결과
+
     #############################################################################
+
     start_coords = (37.646495, 126.739804)  # 시작 좌표
     m = folium.Map(location=start_coords, zoom_start=9, width='100%')
 
     #############################################################################
+
     with open('sudo_geo.json', mode='rt', encoding='utf-8') as f:  # 수도권 지역 geojson 경계선 표시
         geo_sudo = json.loads(f.read())
 
@@ -160,22 +176,25 @@ def home():
         geo_sudo,
         name='sudo_geo'
     ).add_to(m)
+
     #############################################################################
+
     folium_map = MarkerCluster().add_to(m)
 
     if list_area != None:
-        for i in list_area:
-            print(i.latitude, i.longitude)
-            text = "이름: " + str(i.bridge_name) + "\n높이: " + str(i.bridge_height) + "\n수위: " + str(i.WL)
+        for i in range(len(list_area)):
+            text = "이름: " + str(list_area[i].bridge_name) + "\n높이: " + str(list_area[i].bridge_height) + "\n수위: " + str(pred_wl[i])
             pp_text = folium.IFrame(text, width=100, height=150)
-            pp = folium.Popup(pp_text, max_width=1000)
+            pp = folium.Popup(pp_text, max_width=400)
             ic = folium.Icon(color='red', icon='info-sign')
             folium.Marker(
-                location=[i.latitude, i.longitude],
+                location=[list_area[i].latitude, list_area[i].longitude],
                 popup=pp,
                 icon=ic,
             ).add_to(folium_map)
+
     #############################################################################
+
     _ = m._repr_html_()
 
     # get definition of map in body
@@ -184,10 +203,11 @@ def home():
     hdr_txt = Markup(folium_map.get_root().header.render())
     # html to be included in <script>
     script_txt = Markup(folium_map.get_root().script.render())
+
     #############################################################################
 
     return render_template('index.html', status=status, map_div=map_div, hdr_txt=hdr_txt, script_txt=script_txt,
-                           result_rain=rain, result_brid=list_area)
+                           result_rain=rain, result_brid=list_area, now=datetime.today())
 
 
 if __name__ == '__main__':
